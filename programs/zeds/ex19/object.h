fa7ffb02d33929af6d9bc65df42a6cd13de93bf7
leondutoit <leon@comoyo.com>
2014-03-19 21:19:18 +0100

    Another program - a prototype based object system
    Note: does not yet compile for reasons

---
+++ programs/zeds/ex19/object.h
@@ -0,0 +1,36 @@
+// a simple prototype based object system
+
+#ifndef _object_h // "if not defined"
+#define _object_h
+
+typdef enum {
+    NORTH, SOUTH, EAST, WEST
+} Direction;
+
+// a struct with function pointers and such and such
+typdef struct {
+    char *description;
+    int (*init)(void *self);
+    void (*destroy)(void *self);
+    void (*describe)(void *self);
+    void *(*move)(void *self, Direction direction);
+    int (*attack)(void *self, int damage);
+} Object;
+
+int Object_init(void *self);
+void Object_destroy(void *self);
+void Object_describe(void *self);
+void *Object_move(void *self, Direction direction);
+int Object_attack(void *self, int damage);
+void *Object_new(size_t size, Object proto, char *description);
+
+#define NEW(T, N) Object_new(sizeof(T), T##Proto, N)
+// a macro
+// T##Proto -> concat Proto at the end of T
+// E.g. NEW('Leon', 'duToit') -> LeonProto
+#define _(N) proto.N
+// random syntactic sugar (dubious value)
+// obj->proto.ble ~ obj->_(ble)
+// sets the proto of the object
+
+#endif
